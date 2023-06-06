86091596c0379836bfc946ab41f6e0f724e2b3a7
Gaspar Laborde <gasparutn.gmail.com>
2023-06-06 01:04:55 -0300

    video 8 curso avanzado

---
+++ funciones.js
@@ -0,0 +1,9 @@
+function doGet() {
+  return HtmlService.createTemplateFromFile("web")
+    .evaluate()
+    .setTitle("Agenda GoogleApps Script");
+}
+
+function obtenerDatosHTML(nombre) {
+  return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+}
