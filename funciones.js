5f05cc93913cd9560247f29726b7c47b376cca3a
Gaspar Laborde <gasparutn.gmail.com>
2023-06-06 14:49:11 -0300

    video9 curso googleAppsScript

--- funciones.js
+++ funciones.js
@@ -7,3 +7,13 @@ function doGet() {
 function obtenerDatosHTML(nombre) {
   return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos()
+{
+//obtenemos los datos  usando 'openByid' devuelve objeto tipo SpreadSheet y activeSheet devuelve una hoja
+let hoja = SpreadsheetApp.openById('1G1rLBgiinp9kwevEtP6w86JtwWyLlXjrwsqMe6cQUUc').getActiveSheet();
+//obtenemos los datos de tipo dato con DataRange y getValues nos devuelve un objeto de tipo matriz[][]
+let datos = hoja.getDataRange().getValues();
+return datos;
+
+}
